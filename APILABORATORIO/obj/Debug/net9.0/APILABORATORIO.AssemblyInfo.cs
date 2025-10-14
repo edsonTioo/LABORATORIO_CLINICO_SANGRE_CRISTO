41f4ce847efbd0fb84e3497bec48d1342a215cc7
anderguti525 <anderguti525@gmail.com>
2025-10-13 20:12:53 -0600

    corregir el reporte

--- APILABORATORIO/obj/Debug/net9.0/APILABORATORIO.AssemblyInfo.cs
+++ APILABORATORIO/obj/Debug/net9.0/APILABORATORIO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APILABORATORIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad6f5e2e4c30bfbd1deb1541ae61b66a2ea2543")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e372d8cea32d7193e077b2da14af43942a224a70")]
 [assembly: System.Reflection.AssemblyProductAttribute("APILABORATORIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APILABORATORIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
